2f02008df29ae398bec4dc059f8ffcc891303152
Takagi Yuya <y.t.060201@gmail.com>
2019-03-25 16:35:58 +0900

    💄 : Change navbarの背景色等

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,14 +27,17 @@
       height: 500px;
       object-fit: cover;
     }
+    .navbar {
+      background-color: #008080;
+    }
     </style>
 
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/') }}" >
                     {{ config('app.name', 'セルマネ') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
