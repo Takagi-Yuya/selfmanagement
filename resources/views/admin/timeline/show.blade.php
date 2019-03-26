21796f7921968e0c8faa0f466a7d9eaa4e239fe6
Takagi Yuya <y.t.060201@gmail.com>
2019-03-27 00:35:08 +0900

    💄 : Fix / タイムライン詳細画面のviewを編集(show.blade)

--- resources/views/admin/timeline/show.blade.php
+++ resources/views/admin/timeline/show.blade.php
@@ -8,17 +8,16 @@
       <br>
         <div class="card mb-4">
           <div class="card-header">
-            {{ $question->user_id }}さんの質問
+            <p>{{ $question->user_id }}さんの質問</p>
+            <p><b>{{ $question->question }}</b></p>
           </div>
           <div class="card-body">
-            <p class="card-text">
-              {{ $question->question }}
-            </p>
             @if (count($answers) > 0)
               @foreach ($answers as $answer)
-              <p>{{ $answer->user_id }}さんの質問</p>
+              <p>{{ $answer->user_id }}さんの回答</p>
               <p>A.回答：{{ $answer->answer }}</p>
               <p>なぜ？：{{ $answer->reason }}</p>
+              <hr size="3" color="gray">
               @endforeach
             @else
               <p>※まだ回答はありません。</p>
@@ -28,11 +27,6 @@
             <span class="mr-4">
               投稿日時 <small>{{ $question->created_at->format('Y年m月d日') }}</small>
             </span>
-            @if ($answers->count() != 0)
-              <span class="badge badge-primary">
-                コメント {{ $answers->count() }}件
-              </span>
-            @endif
           </div>
         </div>
     </div>
