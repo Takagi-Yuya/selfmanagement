4f640dfad77e1def600e19e703da0747c6120a66
Takagi Yuya <y.t.060201@gmail.com>
2019-04-04 02:00:10 +0900

    🎨 : Change 個別のプロフィールページへリンクする際、自分自身かその他ユーザーかを区別2（if 追加）

--- resources/views/admin/timeline/index.blade.php
+++ resources/views/admin/timeline/index.blade.php
@@ -6,54 +6,54 @@
     <div class="col-md-8 mx-auto">
       <h2>他己分析タイムライン</h2>
       <br>
-        @foreach ($questions as $question)
-          <div class="card mb-4">
-            <div class="card-header">
-              @if ($question->user_id == $user->id)
-                <a href="{{ action('Admin\ProfileController@list', ['id' => $question->user_id])}}">
-              @else
-                <a href="{{ action('Admin\OtherUserProfileController@show', ['id' => $question->user_id])}}">
-              @endif
-              @if ($question->profile == null)
+      @foreach ($questions as $question)
+        <div class="card mb-4">
+          <div class="card-header">
+            @if ($question->user_id == $user->id)
+              <a href="{{ action('Admin\ProfileController@list', ['id' => $question->user_id])}}">
+            @else
+              <a href="{{ action('Admin\OtherUserProfileController@show', ['id' => $question->user_id])}}">
+            @endif
+            @if ($question->profile == null)
+              <p class="image">
+                <img src="{{asset('images/noprofileimage.jpg')}}" alt="" class="image-mini mr-2">{{ $question->user->name }}
+              </p>
+            @else
+              @if ($question->profile['image_path'] == null)
                 <p class="image">
-                  <img src="{{asset('images/noprofileimage.jpg')}}" alt="" class="image-mini mr-2">{{ $question->user->name }}
+                  <img src="{{asset('images/noprofileimage.jpg')}}" alt="" class="image-mini mr-2">{{ $question->profile->name }}
                 </p>
               @else
-                @if ($question->profile['image_path'] == null)
-                  <p class="image">
-                    <img src="{{asset('images/noprofileimage.jpg')}}" alt="" class="image-mini mr-2">{{ $question->profile->name }}
-                  </p>
-                @else
-                  <p class="image">
-                    <img src="{{ asset('storage/image/' . $question->profile->image_path) }}" alt="" class="image-mini mr-2">{{ $question->profile->name }}
-                  </p>
-                @endif
+                <p class="image">
+                  <img src="{{ asset('storage/image/' . $question->profile->image_path) }}" alt="" class="image-mini mr-2">{{ $question->profile->name }}
+                </p>
               @endif
+            @endif
               </a>
-            </div>
-            <div class="card-body">
-              <p class="card-text">
-                <a href="{{ action('Admin\OtherAnswerController@show', ['id' => $question->id]) }}">
-                  <b>Q.質問：{{ $question->question }}</b>
-                </a>
-              </p>
-            </div>
-            <div class="card-footer">
-              <span class="mr-5">
-                <small>{{ $question->created_at->format('Y年m月d日') }}</small>
-              </span>
-              @if ($question->other_answers->count() != 0)
-                <span>
-                  <i class="far fa-comment"></i> {{ $question->other_answers->count() }}
-                </span>
-              @else
-              <span>
-                <i class="far fa-comment"></i> 0
-              </span>
-              @endif
-            </div>
           </div>
-        @endforeach
+          <div class="card-body">
+            <p class="card-text">
+              <a href="{{ action('Admin\OtherAnswerController@show', ['id' => $question->id]) }}">
+                <b>Q.質問：{{ $question->question }}</b>
+              </a>
+            </p>
+          </div>
+          <div class="card-footer">
+            <span class="mr-5">
+              <small>{{ $question->created_at->format('Y年m月d日') }}</small>
+            </span>
+            @if ($question->other_answers->count() != 0)
+              <span>
+                <i class="far fa-comment"></i> {{ $question->other_answers->count() }}
+              </span>
+            @else
+            <span>
+              <i class="far fa-comment"></i> 0
+            </span>
+            @endif
+          </div>
+        </div>
+      @endforeach
     </div>
   </div>
   <div class="d-flex justify-content-center">
--- resources/views/admin/timeline/show.blade.php
+++ resources/views/admin/timeline/show.blade.php
@@ -6,31 +6,39 @@
     <div class="col-md-8 mx-auto">
       <h2>他己分析タイムライン/詳細</h2>
       <br>
-        <div class="card mb-4">
-          <div class="card-header">
+      <div class="card mb-4">
+        <div class="card-header">
+          @if ($question->user_id == $user->id)
+            <a href="{{ action('Admin\ProfileController@list', ['id' => $question->user_id])}}">
+          @else
             <a href="{{ action('Admin\OtherUserProfileController@show', ['id' => $question->user_id])}}">
-            @if ($question->profile == null)
+          @endif
+          @if ($question->profile == null)
+            <p class="image">
+              <img src="{{asset('images/noprofileimage.jpg')}}" alt="" class="image-mini mr-2">{{ $question->user->name }}
+            </p>
+          @else
+            @if ($question->profile['image_path'] == null)
               <p class="image">
-                <img src="{{asset('images/noprofileimage.jpg')}}" alt="" class="image-mini mr-2">{{ $question->user->name }}
+                <img src="{{asset('images/noprofileimage.jpg')}}" alt="" class="image-mini mr-2">{{ $question->profile->name }}
               </p>
             @else
-              @if ($question->profile['image_path'] == null)
-                <p class="image">
-                  <img src="{{asset('images/noprofileimage.jpg')}}" alt="" class="image-mini mr-2">{{ $question->profile->name }}
-                </p>
-              @else
-                <p class="image">
-                  <img src="{{ asset('storage/image/' . $question->profile->image_path) }}" alt="" class="image-mini mr-2">{{ $question->profile->name }}
-                </p>
-              @endif
+              <p class="image">
+                <img src="{{ asset('storage/image/' . $question->profile->image_path) }}" alt="" class="image-mini mr-2">{{ $question->profile->name }}
+              </p>
             @endif
-            </a>
-            <p><b>Q.質問：{{ $question->question }}</b></p>
-          </div>
-          <div class="card-body">
-            @if (count($answers) > 0)
-              @foreach ($answers as $answer)
-              <a href="{{ action('Admin\OtherUserProfileController@show', ['id' => $answer->user_id])}}">
+          @endif
+          </a>
+          <p><b>Q.質問：{{ $question->question }}</b></p>
+        </div>
+        <div class="card-body">
+          @if (count($answers) > 0)
+            @foreach ($answers as $answer)
+              @if ($answer->user_id == $user->id)
+                <a href="{{ action('Admin\ProfileController@list', ['id' => $answer->user_id])}}">
+              @else
+                <a href="{{ action('Admin\OtherUserProfileController@show', ['id' => $answer->user_id])}}">
+              @endif
               @if ($answer->profile == null)
                 <p class="image">
                   <img src="{{asset('images/noprofileimage.jpg')}}" alt="" class="image-mini mr-2">{{ $answer->user->name }}
@@ -46,34 +54,34 @@
                   </p>
                 @endif
               @endif
-              </a>
+                </a>
                 <p>A.回答：{{ $answer->answer }}</p>
                 <p>なぜ？：{{ $answer->reason }}</p>
                 <div class="col-md-11 text-right">
                   @if ($answer->user_id == $user->id)
-                  <a href="{{ action('Admin\OtherAnswerController@edit', ['id' => $answer->id, 'id2' => $question->id]) }}" role='button' class='btn btn-success'>編集</a>
-                  <a href="{{ action('Admin\OtherAnswerController@delete', ['id' => $answer->id]) }}" role='button' class='btn btn-danger'>削除</a>
+                    <a href="{{ action('Admin\OtherAnswerController@edit', ['id' => $answer->id, 'id2' => $question->id]) }}" role='button' class='btn btn-success'>編集</a>
+                    <a href="{{ action('Admin\OtherAnswerController@delete', ['id' => $answer->id]) }}" role='button' class='btn btn-danger'>削除</a>
                   @endif
                 </div>
                 <hr size="3" color="gray">
-              @endforeach
-            @else
-              <p>※まだ回答はありません。</p>
-            @endif
-            @if ($question->user_id != $user->id)
+            @endforeach
+          @else
+            <p>※まだ回答はありません。</p>
+          @endif
+          @if ($question->user_id != $user->id)
             <div class="row">
               <div class="col-md-11 text-right">
                 <a href="{{ action('Admin\OtherAnswerController@add', ['id' => $question->id]) }}" role='button' class='btn btn-success'>新規作成</a>
               </div>
             </div>
-            @endif
-          </div>
-          <div class="card-footer">
-            <span class="mr-4">
-              投稿日時 <small>{{ $question->created_at->format('Y年m月d日') }}</small>
-            </span>
-          </div>
+          @endif
         </div>
+        <div class="card-footer">
+          <span class="mr-4">
+            投稿日時 <small>{{ $question->created_at->format('Y年m月d日') }}</small>
+          </span>
+        </div>
+      </div>
     </div>
   </div>
 </div>
