4b78f29d8faa304035e8c04f9fba0118ed53d450
Takagi Yuya <y.t.060201@gmail.com>
2019-04-12 15:57:22 +0900

    add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
