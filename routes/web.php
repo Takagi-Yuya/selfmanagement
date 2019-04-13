f705b0e64f4b1badd646bd2bc663c6edc59db906
Takagi Yuya <y.t.060201@gmail.com>
2019-04-13 17:56:02 +0900

    testuser login

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/err', function () {
-    trigger_error("エラーのテスト!", E_USER_ERROR);
-});
 
 Route::get('/', function () {
     return view('welcome');
