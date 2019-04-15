3d661fbe31e47574719eb586d4c544b660121fe5
Takagi Yuya <y.t.060201@gmail.com>
2019-04-15 22:05:03 +0900

    defer設定

--- resources/views/layouts/portfolio.blade.php
+++ resources/views/layouts/portfolio.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0" defer></script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" ></script>
 
 
     <!-- Fonts -->
